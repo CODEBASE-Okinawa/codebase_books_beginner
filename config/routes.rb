6088877c6cb61b1e6870f5276788cae76fd86721
reo000 <f-flattune@ezweb.ne.jp>
2023-11-25 18:30:25 +0900

    サインアップ画面作りました

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get '/signup',to:"users#new"
   get '/login', to:"sessions#new"
   post "/login", to:"sessions#create"
   delete "/login", to:"sessions#destroy"
+  resources :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
