3cc8a620121bbe9572361714249cab16bc257440
reo000 <f-flattune@ezweb.ne.jp>
2023-11-26 14:34:51 +0900

    ログイン画面できました

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # user = User.find_by(email: params[:session][:email].downcase)
-    # if user && user.authenticate(params[:session][:password])
-    #     reset session
-
-    # else
-      
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      reset_session    
+      log_in user
+      redirect_to books_path
+    else
+      # flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def destroy
